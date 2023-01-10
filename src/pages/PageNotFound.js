74bdb8f227b61d79ca7557bda06b203131a2eda6
Danting Huang <serena.huangdanting@gmail.com>
2023-01-09 22:35:37 -0500

    update styles for invalid rountes

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/PageNotFound.css";
 
 function PageNotFound() {
   return (
-    <div>
+    <div className="page-not-found-container">
       <h1>Whoops!</h1>
       <div>
         PageNotFound! Please <Link to="/">return to the home page</Link>
