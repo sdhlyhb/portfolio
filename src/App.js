20e5b06977668bc036c8bf7906e606e863cb9144
Danting Huang <serena.huangdanting@gmail.com>
2023-01-09 22:39:21 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about-me" element={<AboutMe />} />
           <Route path="/*" element={<PageNotFound />} />
